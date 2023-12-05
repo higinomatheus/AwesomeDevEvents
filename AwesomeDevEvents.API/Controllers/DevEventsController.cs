ccf2f5abc7cbdac05b7aa7cbb656759757d21880
matheushigino_nepreconsulting <matheus.higino@nepreconsulting.com>
2023-12-05 10:23:01 -0300

    Criação de CRUD com ASP.NET Core 7

---
+++ AwesomeDevEvents.API/Controllers/DevEventsController.cs
@@ -0,0 +1,99 @@
+﻿using AwesomeDevEvents.API.Entities;
+using AwesomeDevEvents.API.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwesomeDevEvents.API.Controllers
+{
+    [Route("api/dev-events")]
+    [ApiController]
+    public class DevEventsController : ControllerBase
+    {
+        private readonly DevEventsDbContext _context;
+
+        public DevEventsController(DevEventsDbContext context)
+        {
+            _context = context;    
+        }
+
+        // api/dev-events GET
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var devEvents =_context.DevEvents.Where(d => !d.IsDeleted).ToList();
+
+            return Ok(devEvents);
+        }
+
+        // api/dev-events/3fa85f64-5717-4562-b3fc-2c963f66afa6 GET
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if(devEvent == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(devEvent);
+        }
+
+        // api/dev-events POST
+        [HttpPost]
+        public IActionResult Post(DevEvent devEvent)
+        {
+            _context.DevEvents.Add(devEvent);
+
+            return CreatedAtAction(nameof(GetById), new { id = devEvent.Id }, devEvent);
+        }
+
+        // api/dev-events/3fa85f64-5717-4562-b3fc-2c963f66afa6 PUT
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id, DevEvent input)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if(devEvent == null)
+            {
+                return NotFound();
+            } 
+
+            devEvent.Update(input.Title, input.Description, input.StartDate, input.EndDate);
+            
+            return NoContent();
+        }
+
+        // api/dev-events/3fa85f64-5717-4562-b3fc-2c963f66afa6 DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if(devEvent == null)
+            {
+                return NotFound();
+            }
+
+            devEvent.Delete();
+
+            return NoContent();
+        }
+
+        // api/dev-events/3fa85f64-5717-4562-b3fc-2c963f66afa6 POST
+        [HttpPost("{id}/speakers")]
+        public IActionResult PostSpeaker(Guid id, DevEventSpeaker speaker)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if(devEvent is null)
+            {
+                return NotFound();
+            }
+
+            devEvent.Speakers.Add(speaker);
+
+            return NoContent();
+        }
+    }
+}
